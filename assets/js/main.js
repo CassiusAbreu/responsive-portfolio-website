e360cc7675eb759bcb57f9b83945bcffce4e4bac
vini <cassiusabreu1@gmail.com>
2022-12-02 09:31:20 -0300

    swipper

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,3 +88,17 @@ modalCloses.forEach((modalClose) => {
     })
 })
 })
+
+/*========================= SERVICES MODAL =====================*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
